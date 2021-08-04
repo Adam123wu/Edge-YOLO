fa4c2a2c524c0fe00528d2814042ebaba64b97de
Adam123wu <48359834+Adam123wu@users.noreply.github.com>
2021-08-04 23:00:30 +0800

    Add files via upload

---
+++ MobileNet V3 SSD/create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/home/jx/darknet/VOC',
+                      voc12_path='',
+                      output_folder='output')
